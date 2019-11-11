487132c09c05fe0d4021b7618663692509f30dfa
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-11-11 20:31:44 +0100

    Update 001_plot_dendro.R

--- R/src/001_plot_dendro.R
+++ R/src/001_plot_dendro.R
@@ -24,6 +24,7 @@ source(file.path(root_folder, paste0(pathdir,"000_envrmt_bio_v1.R")))
 
 # developmnet script
 
+# standort mittelkurve 
 # read csv
 df <- read.csv(file.path(envrmt$path_dendro,"data_a1.csv"))
 df[206,]
@@ -35,7 +36,7 @@ dfn <- dfrow[,-1]
 # delete unused cols
 dfcol <- dfn[,1:18]
 
-#calc mean per row
+#calc mean per row (Standortmittelkurve)
 dfcol$mean <- rowSums(dfcol,na.rm = T)/18
 #plot with ts plot (rep() repeates a string)
 ts.plot(ts(dfcol[1:18]),
@@ -44,6 +45,47 @@ ts.plot(ts(dfcol[1:18]),
         ylab="mm",
         main="yearring width", lwd=2)
 
+# preprocess df ##########################################################################################
+df <- read.csv(file.path(envrmt$path_dendro,"data_a1.csv"))
+#delete unused rows
+dfrow <- df[206:(nrow(df)-2),]
+#get names from first col and delet first col
+rownames(dfrow) <- dfrow[,1]
+dfn <- dfrow[,-1]
+# delete unused cols
+dfcol <- dfn[,1:18]
+dfcol
+
+# calc n trees per year (belegungsdichte) ################################################################
+for (i in 1:nrow(dfcol)) {
+  dfcol[i,19] <-length(which(dfcol[i,]>0))
+}
+dfcol[,19]
+
+#########################################################################################################
+
+### durchscnittliches wachstum, für standart datensatz
+#für darstellung mit baumittelwerten (also colsums pro col für baum)
+df <- read.csv(file.path(envrmt$path_dendro,"data_a1.csv"))
+#delete unused rows
+dfrow <- df[206:(nrow(df)-2),]
+dfrow <-df
+#get names from first col and delet first col
+rownames(dfrow) <- dfrow[,1]
+dfn <- dfrow[,-1]
+# delete unused cols
+dfcol <- dfn[,1:18]
 dfcol
 
 
+# calc mean per col (mittleres wachstum pro baum ) ################################################################
+lastrow <-nrow(dfcol)+1 #must be outside loop to prevent addition of 1 per tick
+for (i in 1:ncol(dfcol)) {
+  
+  dfcol[lastrow,i] <-mean(dfcol[,i],na.rm=T)
+}
+# mittleres wachstum aller bäume
+MW <- sum(dfcol[lastrow,])/ncol(dfcol)
+MW
+)
+###################################################################################################################
