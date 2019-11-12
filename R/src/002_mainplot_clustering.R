c15e7c2e64363c50a1c84ae495452e9c7c9fa3e3
johnfurie <44396278+johnfurie@users.noreply.github.com>
2019-11-12 14:02:26 +0100

    update plot clean

--- R/src/002_mainplot_clustering.R
+++ R/src/002_mainplot_clustering.R
@@ -28,18 +28,38 @@ source(file.path(root_folder, paste0(pathdir,"000_envrmt_bio_v1.R")))
 source(file.path(envrmt$path_REAVER_hyperspace,"dev_plot_hyperspace.R"))
 
 # load testdata
-list.files(envrmt$path_org)
-df <- read.table(file.path(envrmt$path_org,"xxx"),header = T)
+
+df <- read.csv(file.path(envrmt$path_org,"plotcsv_ohneBG.csv"),header = T)
+df <- read.csv(file.path(envrmt$path_org,"plotcsv.csv"),header = T)
 df
 
+###long to wide
+dfwide <-reshape(df, direction = "wide", idvar = "Plot.ID", timevar = "Art")
+
+### replace NA to 0
+dfwide[is.na(dfwide)] <- 0
+
+rownames(dfwide) <- dfwide$Plot.ID
+
+##
+#normalverteilt - t-test
+#nicht normalverteilt - smirnoff
+#gruppen -anova()
+#metrische daten - regression
+
+#1. arten (gesamtliste) (allgemein descriptiv)
+#2. gesellschaft nach waldtyp
+#3. Artspezifisch (waldtyp substrat)
+###exclude first row
+dfw <-df[,-1]
 ###############################################################################################
 
 #boxplot data
-boxplot(df)
+boxplot(dfwide)
 
 # check hc cluster
 par(mfrow=c(1,1))
-vdist <- vegdist(df, method = "bray", binary = FALSE)
+vdist <- vegdist(dfw, method = "bray", binary = FALSE)
 cluster <- hclust(vdist, method = "ward.D")
 plot(cluster, hang = -1)
 
@@ -51,5 +71,5 @@ rect.hclust(cluster,6, border ="violet")
 rect.hclust(cluster,7, border ="brown")
 
 # set desired clusters
-test <-Reaver_plot_hyperspace(df)
+test <-Reaver_plot_hyperspace(dfw,cl=4,indi=T)
 
