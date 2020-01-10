3d65d4f4c388bf590acdd3a1a332d5a40a2c3942
SchoenbergA <Schoenb4@students.uni-marburg.de>
2020-01-10 22:25:48 +0100

    scr for perfoming ReaverHyperspace
    contains loading of all privots and fucntion to delete species

---
+++ R/src/004_Plot_hyperspace.R
@@ -0,0 +1,90 @@
+#############################################################################################
+###--- Setup Environment -------------------------------------------------------------------#
+###############################################           #
+# require libs for setup          #EEEE n   n v       v rrrr    m     m   ttttt #           #                  
+require(raster)                   #E    nn  n  v     v  r   r  m m   m m    t   #           #         
+require(envimaR)                  #EE   n n n   v   v   rrrr   m m   m m    t   #           #                
+require(link2GI)                  #E    n  nn    v v    r  r  m   m m   m   t   #           #             
+#EEEE n   n     v     r   r m    m    m   t   #           #
+###############################################           #
+
+# define needed libs and src folder                                                         #
+libs = c("link2GI","reshape2","dplyr","vegan","cluster","labdsv","rgdal","stringr") 
+pathdir = "R/src/"
+
+#set root folder for uniPC or laptop                                                        #
+root_folder = alternativeEnvi(root_folder = "~/edu/Envimaster-BioGeo",                      #
+                              alt_env_id = "COMPUTERNAME",                                  #
+                              alt_env_value = "PCRZP",                                      #
+                              alt_env_root_folder = "F:/edu/Envimaster-BioGeo")             #
+#source environment script                                                                  #
+source(file.path(root_folder, paste0(pathdir,"000_envrmt_bio_v1.R")))                                                              
+###---------------------------------------------------------------------------------------###
+#############################################################################################
+
+# perform Multivariant statistics
+# Script to run several MVS approches on the different privot tables
+
+# source Function
+source(file.path(envrmt$path_REAVER_hyperspace,"dev_plot_hyperspace.r"))
+
+#load data
+SL <-read.csv(file.path(envrmt$path_privot,"soil_cov.csv"),row.names = 1)
+DW <-read.csv(file.path(envrmt$path_privot,"deadwood_cov.csv"),row.names = 1)
+EP <-read.csv(file.path(envrmt$path_privot,"epi_cov.csv"),row.names = 1)
+MP <-read.csv(file.path(envrmt$path_privot,"mainplots_cov.csv"),row.names = 1)
+SU <-read.csv(file.path(envrmt$path_privot,"substrates_cov.csv"),row.names = 1)
+
+sl <-read.csv(file.path(envrmt$path_privot,"soil_occur.csv"),row.names = 1)
+dw <-read.csv(file.path(envrmt$path_privot,"deadwood_occur.csv"),row.names = 1)
+ep <-read.csv(file.path(envrmt$path_privot,"epi_occur.csv"),row.names = 1)
+mp <-read.csv(file.path(envrmt$path_privot,"mainplots_occur.csv"),row.names = 1)
+su <-read.csv(file.path(envrmt$path_privot,"substrates_occur.csv"),row.names = 1)
+
+# plot Reaver Hyperspace
+
+# for interpretation of nmds stress level:
+# over 0.2 caution, over 0.3 highly suspect
+
+# accumulated cover
+Reaver_plot_hyperspace(SL,3,display = "hc_nmds")
+Reaver_plot_hyperspace(DW,3)
+Reaver_plot_hyperspace(EP,3)
+Reaver_plot_hyperspace(MP,3)
+Reaver_plot_hyperspace(SU,3)
+
+
+# occuriance only
+Reaver_plot_hyperspace(sl,3)
+Reaver_plot_hyperspace(dw,3)
+Reaver_plot_hyperspace(ep,3)
+Reaver_plot_hyperspace(mp,3)
+Reaver_plot_hyperspace(su,3)
+
+### modifie tables
+
+# detect dominant species
+boxplot(SL,las=3) # none
+boxplot(DW,las=3) #hyp cup, bra rut
+boxplot(EP,las=3) #hyp cup
+boxplot(MP,las=3) #hyp cup, bra rut
+boxplot(SU,las=3) #hyp cup
+# not necceserie for only occourence
+
+colnames(DW)
+DW_clean <- delete.species(DW,"hypnum.cupressiforme")
+DW_clean <- delete.species(DW_clean,"brachythecium.rutabulum")
+
+EP_clean <- delete.species(EP,"hypnum.cupressiforme")
+
+MP_clean <- delete.species(MP,"hypnum.cupressiforme")
+MP_clean <- delete.species(MP_clean,"brachythecium.rutabulum")
+
+SU_clean <- delete.species(SU,"hypnum.cupressiforme")
+
+#run hyperspace for cleand tabs
+Reaver_plot_hyperspace(SL,3,display = "hc_nmds")
+Reaver_plot_hyperspace(DW_clean,2)
+Reaver_plot_hyperspace(EP_clean,3,display = "hc_nmds")
+Reaver_plot_hyperspace(MP_clean,3,display = "hc_nmds")
+Reaver_plot_hyperspace(SU_clean,3,display = "hc_nmds")
