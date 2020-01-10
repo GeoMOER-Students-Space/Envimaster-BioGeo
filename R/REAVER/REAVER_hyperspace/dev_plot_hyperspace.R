4f5212d65957fa91acc21e1cc4972e7c7dceb8a3
SchoenbergA <Schoenb4@students.uni-marburg.de>
2020-01-10 22:09:59 +0100

    update  added display argument
    now via argumnet display a single print can be performed for desired combinaten of clustering an orinatio methode
    
    further added warings if stress level for nmds ist critical

--- R/REAVER/REAVER_hyperspace/dev_plot_hyperspace.R
+++ R/REAVER/REAVER_hyperspace/dev_plot_hyperspace.R
@@ -9,14 +9,15 @@
 #' @param Mandatory if function: cl - number of desired cluster, default = TRUE
 #' @param Mandatory if function: indi - if TRUE the indicator parameters are printed, default = TRUE
 #' @param Mandatory if function: re - if TRUE the clusters for hc and km are returned, default = FALSE
-
+#' @param Mandatory if function: display - the desired combination (ordination and clustering), use "hc_mnds", "km_nmds", "hc_dca" or "km_dca" to be displayed.
+#' if missing, a 2x2 plot will be printen with HC and KM Clustering with each nmds and dca.
 #note: v1.2 returning improved cluster quality parameters
 # stats for HC only !!!
 
 # add stress level for nmds
 
 
-Reaver_plot_hyperspace <-function(df,cl,indi=FALSE,re=FALSE){
+Reaver_plot_hyperspace <-function(df,cl,indi=FALSE,re=FALSE,display="default"){
   if (missing(cl)){
     stop("missing argument cl with no default")
   }
@@ -30,7 +31,9 @@ Reaver_plot_hyperspace <-function(df,cl,indi=FALSE,re=FALSE){
   nmds<-metaMDS(df)
   
   print(nmds$stress)
-  print(nmds$grstress)
+  if(nmds$stress>0.2) {warning("Stress over 0.2 !")}
+  if(nmds$stress>0.3) {warning("Stress over 0.3 ! Results are highly speculativ")}
+  #print(nmds$grstress)
  
   #clusters#########################################################################################
   #bray ward
@@ -40,44 +43,79 @@ Reaver_plot_hyperspace <-function(df,cl,indi=FALSE,re=FALSE){
   #kmeans clustering
   km_cl <- kmeans(df,centers=cl,nstart=20)
   #plot
-  par(mfrow=c(2,2))
-  
+  if(display=="hc_nmds"){
   #plot with hc nmds
   sc<-scores(nmds)
   ordiplot(nmds,type="n",main="hc_nmds")
   orditorp(nmds,display="sites",cex=1,air=0.01)
   points(sc[,1],sc[,2],cex=2,pch=20,col=cutclust)
   ordihull(nmds, cutclust, lty=2, col="blue")
-  
+  }
+  if(display=="km_nmds"){
   #plot with km nmds 
   ordiplot(nmds,type="n",main="km_nmds")
   orditorp(nmds,display="sites",cex=1,air=0.01)
   ordihull(nmds, km_cl$cluster, lty=3, col="grey60",lwd=2)
   points(sc[,1],sc[,2],cex=2,pch=20,col=km_cl$cluster)
-
+  }
+  if(display=="hc_dca"){
   #plot with hc dca points
   scd<-scores(dca)
   plot(dca,display="sites",type="n", main="hc_dca")
   orditorp(dca,display="sites",cex=1,air=0.01)
   points(scd[,1],scd[,2],cex=2,pch=20,col=cutclust)
   ordihull(dca, cutclust, lty=2, col="blue")
-  
+  }
+  if(display=="km_dca"){
   #plot with km dca points
   plot(dca,display="sites",type="n", main="km_dca")
   orditorp(dca,display="sites",cex=1,air=0.01)
   points(scd[,1],scd[,2],cex=2,pch=20,col=km_cl$cluster)
   ordihull(dca, km_cl$cluster, lty=3, col="grey60",lwd=2)
-  
+  }
+  if(display=="default"){
+    par(mfrow=c(2,2))
+    
+      #plot with hc nmds
+      sc<-scores(nmds)
+      ordiplot(nmds,type="n",main="hc_nmds")
+      orditorp(nmds,display="sites",cex=1,air=0.01)
+      points(sc[,1],sc[,2],cex=2,pch=20,col=cutclust)
+      ordihull(nmds, cutclust, lty=2, col="blue")
+    
+      #plot with km nmds 
+      ordiplot(nmds,type="n",main="km_nmds")
+      orditorp(nmds,display="sites",cex=1,air=0.01)
+      ordihull(nmds, km_cl$cluster, lty=3, col="grey60",lwd=2)
+      points(sc[,1],sc[,2],cex=2,pch=20,col=km_cl$cluster)
+   
+      #plot with hc dca points
+      scd<-scores(dca)
+      plot(dca,display="sites",type="n", main="hc_dca")
+      orditorp(dca,display="sites",cex=1,air=0.01)
+      points(scd[,1],scd[,2],cex=2,pch=20,col=cutclust)
+      ordihull(dca, cutclust, lty=2, col="blue")
+    
+      #plot with km dca points
+      plot(dca,display="sites",type="n", main="km_dca")
+      orditorp(dca,display="sites",cex=1,air=0.01)
+      points(scd[,1],scd[,2],cex=2,pch=20,col=km_cl$cluster)
+      ordihull(dca, km_cl$cluster, lty=3, col="grey60",lwd=2)
+      par(mfrow=c(1,1))
+    
+  }
  # #indicator for hc
   if (indi==TRUE){
   const_hc <-const(df, cutclust)
   import_hc <-importance(df, cutclust,show=NA)
   hc_ival <- indval(df, cutclust)
+  print("Summary Indicators for HC")
   summary(hc_ival)  
   
   const_km <-const(df, km_cl$cluster)
   import_km <-importance(df,km_cl$cluster,show=NA)
   km_ival <- indval(df, km_cl$cluster) # summarys indicator
+  print("Summary Indicators for KM")
   summary(km_ival)
   }
   ####################################################################
