c002c93c4a469b50143d45cf7a51a0ccd44ad536
SchoenbergA <Schoenb4@students.uni-marburg.de>
2020-04-06 20:42:47 +0200

    add new function
    add function to modifie the output plots for MVS results

--- R/src/functions_src.R
+++ R/src/functions_src.R
@@ -126,4 +126,38 @@ ind_groups <- function(df){
   summary(hc_ival)  
     }
 
+# print orination plots function 
+
+#lcex= cex for the labels of points
+#pcex= cex for ponits
+#mcex= cex for maintitel
+#acex= cex for axis
+#lwd= cluster line witdt
+
+print_hc_nmds <-function(df,cl,lcex=1,pcex=2,mcex=1,acex=1,lwd=1, air=1,main="name"){
+  #input check
+  if (missing(cl)){
+    stop("missing argument cl with no default")
+  }
+  
+  nmds<-metaMDS(df)
+  # nmds stress level
+  print(nmds$stress)
+  if(nmds$stress>0.2) {warning("Stress over 0.2 !")}
+  if(nmds$stress>0.3) {warning("Stress over 0.3 ! Results are highly speculativ")}
+  # cluster
+  vdist <- vegdist(df, method = "bray", binary = FALSE)
+  cluster <- hclust(vdist, method = "ward.D")
+  cutclust <- cutree(cluster, k=cl)
+  #plot with hc nmds
+  sc<-scores(nmds)
+  ordiplot(nmds,type="n",main=main, 
+           cex.main=mcex,
+           cex.axis=acex,
+           mar=c(1,1,1,1))
+  orditorp(nmds,display="sites",cex=lcex)
+  text(x=1,y=-1.2,cex=1,label=paste0("nmds stress level: ",round(nmds$stress,4)))
+  points(sc[,1],sc[,2],cex=pcex,pch=20,col=cutclust)
+  ordihull(nmds, cutclust, lty=2,lwd=lwd, col="blue")
+}
 
